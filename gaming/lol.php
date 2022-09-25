bbc80ea5d27bae241583660843d05f54cd5dbfb1
HarryBirds <104190055+HarryBirds@users.noreply.github.com>
2022-09-25 22:43:45 +0100

    Add files via upload

---
+++ gaming/lol.php
@@ -0,0 +1,23 @@
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Links | Harry B</title>
+        <link rel="icon" type="image/x-icon" href="img/icon.png">
+        <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <link rel="stylesheet" href="style.css">
+        <div class="content">
+
+            <!-- Picture -->
+
+            <img src="img/image.jpg" alt="Photo" class=" logo wdn-stretch rounden">
+            <h1>Your IP Address is: <? echo $_SERVER["REMOTE_ADDR"]; ?></h1>
+
+
+            <!--<h2>Other Links</h2>-->
+        </div>
+    </body>
+</html>
